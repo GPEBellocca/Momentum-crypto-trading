8840f65e92f5fb0eff6b76eb7a2b06d5f29de63e
GPEBellocca <gpebellocca@gmail.com>
2021-04-19 22:13:47 +0200

    port_lib

--- CTS/portfolio_library.py
+++ CTS/portfolio_library.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import datetime as dt
 from pandas_datareader import data
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import *
-import matplotlib.ticker as mtick
+#import matplotlib
+#matplotlib.use('TkAgg')
+#import matplotlib.pyplot as plt
+#from matplotlib.pyplot import *
+#import matplotlib.ticker as mtick
 import numpy as np
 import statistics as stat
 from statistics import stdev
